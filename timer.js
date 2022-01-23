c8d99b655346a1878a06dacb6d6af1c2a5d83f82
deepaktho <deepakthorat900@gmail.com>
2022-01-23 09:17:47 +0530

    .

--- timer.js
+++ timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 26, 2021 23:00:00").getTime();
+var countDownDate = new Date("Jan 23, 2022 23:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
